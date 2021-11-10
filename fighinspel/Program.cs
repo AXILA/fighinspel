41fa7b78594aef0569cc5c36b3ad0234c4d4b4e5
AxelRysjöLisslö <rysjolisslo.axel@elev.ga.ntig.se>
2021-11-10 13:25:45 +0100

    initial commit

--- fighinspel/Program.cs
+++ fighinspel/Program.cs
@@ -1,4 +1,26 @@
 ﻿using System;
 
-int hpPlayerOne = 100;
-int hpPlayerTow = 100;
+int playerOneHp = 100;
+int playerTowHp = 100;
+
+Random generator = new Random();
+
+while (playerOneHp > 0 && playerTowHp > 0)
+{
+Console.WriteLine();
+Console.WriteLine();
+
+
+int PlayerOneDamage = generator.Next(15);
+playerTowHp -= PlayerOneDamage;
+playerTowHp = Math.Max(0,playerTowHp);
+Console.WriteLine();
+
+int playerTowDamage = generator.Next(15);
+playerOneHp -= playerTowDamage;
+playerOneHp = Math.Max(0,playerOneHp);
+
+Console.WriteLine();
+Console.ReadKey();
+}
+
